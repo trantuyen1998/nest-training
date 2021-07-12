48b9fde0916d7c0d46e994e12b4552339961f1dd
tuyendev2505 <tuyen.dev2505@gmail.com>
2021-07-12 16:45:07 +0700

    add create task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  // @Body() task -> pass a object
+  @Post()
+  createTask(@Body('title') title, @Body('description') description) {
+    return this.tasksService.createTask(title, description);
+  }
+}
